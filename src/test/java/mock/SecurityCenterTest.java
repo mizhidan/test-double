6b8f70a6082dd675d6776f76805583069d6ff38d
unknown <dj9824@126.com>
2020-08-05 09:11:47 +0800

    [danjie] feat: GradeService & SecurityCenter

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,17 +3,25 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
 
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-
+    @Mock
+    DoorPanel doorPanel;
+    SecurityCenter securityCenter;
     @BeforeEach
     public void setUp() {
+        doorPanel = mock(DoorPanel.class);
+        securityCenter = new SecurityCenter(doorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,34 @@
-package stub;
+package mockito;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+import stub.GradeService;
+import stub.GradeSystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GradeServiceTest {
-    /* 需求描述：
-    编写GradeService类的单元测试，单元测试calculateAverageGrades方法
-    * */
+
+    private GradeService gradeService;
+
+    @BeforeEach
+    public void setUp() {
+        GradeSystem gradeSystem = mock(GradeSystem.class);
+        when(gradeSystem.gradesFor(1)).thenReturn(Arrays.asList(90.0, 90.0, 90.0));
+        gradeService = new GradeService(gradeSystem);
+    }
 
     @Test
-    public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+    public void calculate_student_avg_score() {
+        assertEquals(gradeService.calculateAverageGrades(1), 90.0);
     }
+
+
+
 }
